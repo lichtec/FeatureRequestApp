7bd1a1f67bc988c095fcfec186de936175120b54
chris.lichter <chris.lichter@gmail.com>
2017-06-14 17:29:39 -0400

    Rough frontend in place. Need to add CUD

---
+++ app/static/js/formControlls.js
@@ -0,0 +1,15 @@
+// function pageLoad() {
+//   var navEl = document.getElementById("navBar");
+//   navEl.addEventListener("click", navControls)
+// }
+
+// function navControls(e) {
+//   var targetEl = e.target;
+//   var curActive = document.getElementsByClassName("active");
+//   curActive = curActive[0];
+//   curActive.setAttribute("class", "");
+//   targetEl.setAttribute("class", "active");
+  
+// }
+
+// window.addEventListener("load", pageLoad, "False");
--- app/static/js/mainFeatures.js
+++ app/static/js/mainFeatures.js
@@ -1,17 +1,3 @@
-var initialData = [{
-  client_id: 1,
-  client_name: "Topher",
-  description: "Please add a front-end",
-  id: 1,
-  priority: 1,
-  productArea_id: 1,
-  productArea_name: "Front-end",
-  submitter_id: 1,
-  submitter_name: "Chris Lichter",
-  targetDate: "01-01-2018",
-  title: "Front-end"
-}]
-
 function Feature(data) {
   this.id = ko.observable(data.id);
   this.title = ko.observable(data.title);
@@ -26,9 +12,41 @@ function Feature(data) {
   this.submitter_name = ko.observable(data.submitter_name);
 }
 
+function Client(data) {
+  this.id = ko.observable(data.id);
+  this.client_name = ko.observable(data.client_name);
+}
+
+function User(data) {
+  this.id = ko.observable(data.id);
+  this.user_name = ko.observable(data.user_name);
+}
+
+function ProductArea(data) {
+  this.id = ko.observable(data.id);
+  this.productArea_name = ko.observable(data.productArea_name);
+}
+
 var FeatureModel = function() {
   var self = this;
+  self.navs = ['Features', 'Clients', 'Users', 'Product Areas'];
+  self.chosenNavId = ko.observable();
+
   self.featuresArray = ko.observableArray([]);
+  self.featureCols = ['Id', 'Title', 'Description', 'Client Id', 'Client Name', 'Priority', 'Target Date', 'Product Area Id', 'Product Area Name', 'Submitter Id', 'Submitter Name'];
+
+  self.clientsArray = ko.observableArray([]);
+  self.clientCols = ['Id', 'Client Name'];
+
+  self.usersArray = ko.observableArray([]);
+  self.userCols = ['Id', 'User Name'];
+  
+  self.productAreasArray = ko.observableArray([]);
+  self.productAreaCols = ['Id', 'User Name'];
+
+  self.goToNav = function(nav) {
+    self.chosenNavId(nav);
+  };
 
   $.getJSON("/features", function(data) {
     var mappedFeatures = $.map(data.features, function(item) {
@@ -36,6 +54,25 @@ var FeatureModel = function() {
     });
     self.featuresArray(mappedFeatures);
   });
+  $.getJSON("/clients", function(data) {
+    var mappedClients = $.map(data.clients, function(item) {
+      return new Client(item)
+    });
+    self.clientsArray(mappedClients);
+  });
+  $.getJSON("/users", function(data) {
+    var mappedUsers = $.map(data.users, function(item) {
+      return new User(item)
+    });
+    self.usersArray(mappedUsers);
+  });
+  $.getJSON("/product_areas", function(data) {
+    var mappedProductAreas = $.map(data.product_areas, function(item) {
+      return new ProductArea(item)
+    });
+    self.productAreasArray(mappedProductAreas);
+  });
+  self.goToNav('Features');
 };
 
 ko.applyBindings(new FeatureModel());
