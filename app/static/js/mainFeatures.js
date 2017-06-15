a4bbb6a32802da419c21ed19ec3b728319e053e4
chris.lichter <chris.lichter@gmail.com>
2017-06-15 17:34:43 -0400

    Feature and client add are working

--- app/static/js/mainFeatures.js
+++ app/static/js/mainFeatures.js
@@ -34,18 +34,66 @@ var FeatureModel = function() {
 
   self.featuresArray = ko.observableArray([]);
   self.featureCols = ['Id', 'Title', 'Description', 'Client Id', 'Client Name', 'Priority', 'Target Date', 'Product Area Id', 'Product Area Name', 'Submitter Id', 'Submitter Name'];
+  self.newFeatureTitle = ko.observable();
+  self.newFeatureDescription = ko.observable();
+  self.newFeatureClient_name = ko.observable();
+  self.newFeaturePriority = ko.observable();
+  self.newFeatureTargetDate = ko.observable();
+  self.newFeatureProductArea_name = ko.observable();
+  self.newFeatureSubmitterName = ko.observable();
 
   self.clientsArray = ko.observableArray([]);
   self.clientCols = ['Id', 'Client Name'];
+  self.newClientName = ko.observable();
 
   self.usersArray = ko.observableArray([]);
   self.userCols = ['Id', 'User Name'];
-  
+  self.newUserName = ko.observable();
+
   self.productAreasArray = ko.observableArray([]);
   self.productAreaCols = ['Id', 'User Name'];
+  self.newProductAreaName = ko.observable();
 
   self.goToNav = function(nav) {
     self.chosenNavId(nav);
+  };
+
+  self.addFeature = function() {
+    newFeature = new Feature({
+      title: this.newFeatureTitle(),
+      description: this.newFeatureDescription(),
+      client_name: this.newFeatureClient_name(),
+      priority: this.newFeaturePriority(),
+      targetDate: this.newFeatureTargetDate(),
+      productArea_name: this.newFeatureProductArea_name(),
+      submitter_name: this.newFeatureSubmitterName()
+    });
+    self.featuresArray.push(newFeature);
+    $.ajax("/add_feature", {
+      data: ko.toJSON(newFeature),
+      type: "post",
+      contentType: "application/json"
+    });
+    self.newFeatureTitle("");
+    self.newFeatureDescription("");
+    self.newFeatureClient_name("");
+    self.newFeaturePriority("");
+    self.newFeatureTargetDate("");
+    self.newFeatureProductArea_name("");
+    self.newFeatureSubmitterName("");
+  };
+  
+  self.addClient = function() {
+    newClient = new Client({
+      client_name: this.newClientName()
+    });
+    self.clientsArray.push(newClient);
+    $.ajax("/add_client", {
+      data: ko.toJSON(newClient),
+      type: "post",
+      contentType: "application/json"
+    });
+    self.newClientName("");
   };
 
   $.getJSON("/features", function(data) {
